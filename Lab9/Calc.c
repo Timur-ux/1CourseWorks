60ae74ea8f00d66e6fee65d3b67136b1e37ddda8
Head_Timuroves <timur.buchkin@mail.ru>
2022-10-25 23:00:22 +0300

    Доделал 9 лабу

---
+++ Lab9/Calc.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+//Данный модуль содержит всю логику и математику программы.
+//Определяем основные функции(математические). Макс, мин, проверить на попадание, знак, модуль
+int max(int a, int b){
+    return (a>b) ? a : b;
+}
+int min(int a, int b){
+    return (a>b) ? b : a;
+}
+int chek(int i, int j, int Ci, int Cj, int A, int B){
+    if (abs(Cj - j)>A){
+        return 0;
+    }
+    //Xo2mCi - максимальный разлёт значений х от Ci, если у считать параметром.(i ~ x, j ~ y в дальнейшем).
+    int Xo2mCi = A*A*(1-(j-Cj)*(j-Cj)/B/B);
+    if(Xo2mCi >= (i-Ci)*(i-Ci)){
+        return 1;
+    }
+    return 0;
+}
+
+int sign(int n){
+    if (n > 0) return 1;
+    if (n == 0) return 0;
+    return -1;
+}
+int abs(int x){
+    if (x >= 0) return x;
+    return -x;
+}
+//Главная вычислительная функция, для данного теста вычисляет попадание.
+int Calc(const int i0, const int j0, const int l0){
+    //Объявление начальных констант
+    const int Ci = 10;
+    const int Cj = 10;
+    const int A = 10;
+    const int B = 5;
+    //Объявление начальных переменных
+    int ik = i0;
+    int jk = j0;
+    int lk = l0;
+    //Проверка на попадание при k = 0, затем при k = 1,2,...,50
+    if(chek(i0, j0, Ci, Cj, A, B)){
+        printf("Yes. k = 0, i = %d, j = %d, l = %d\n", i0, j0, l0);
+        return 0;
+    }
+    for(int k = 1; k <= 50; k++){
+        int copyik = ik, copyjk = jk, copylk = lk;//Создание копий ik, jk, lk
+        //Обновляем ik, jk, lk
+        ik = copyik*max(copyjk, copylk)%30 + copyjk*min(copyik, copylk)%20 + k;
+        jk = min(copyjk, max(copyjk,min(copylk, max(copyjk-copylk, copyik-copylk))));
+        lk = sign(k-10)*abs(copyik-copyjk+copylk-k);
+        //Проверка на попадание
+        if(chek(ik, jk, Ci, Cj, A, B)){
+            printf("Yes. k = %d, i = %d, j = %d, l = %d\n", k, ik, jk, lk);
+            return 0;
+        }
+    }
+    //Если вышли из цикла, значит промах.
+    printf("No. k = 50, i = %d, j = %d, l = %d\n", ik, jk, lk);
+    return 0;
+}
--- Lab9/main.c
+++ Lab9/main.c
@@ -1,57 +1,23 @@
+//Лабораторная работа №9. Вариант 29. Бучкин Тимур Артемович. М8О-101Б-22.
+//Здесь содержатся тесты, реализация программы находится в модуле Calc.c
 #include <stdio.h>
-#include <stdbool.h>
-int max(int a, int b){
-    return (a>b) ? a : b;
-}
-int min(int a, int b){
-    return (a>b) ? b : a;
-}
-bool chek(int Rk2, int Rl, int Rb, int k){
-    if ((Rk2 >= sqr(Rl)) & (Rk2 <= sqr(Rb))){
-        printf("Yes! k = %d", k);
-        return true;
-    }
-    return false;
-}
+#include "Calc.c"
 
-int sign(int n){
-    if (n > 0) return 1;
-    if (n == 0) return 0;
-    return -1;
-}
-int abs(int x){
-    if (x >= 0) return x;
-    return -x;
-}
-int sqr(int a){
-    return a*a;
-}
 int main(){
-    const int Rl = 5;
-    const int Rb = 10;
-    const int Ci = 10;
-    const int Cj = 10;
-    const int i0 = 18;
-    const int j0 = -9;
-    const int l0 = 5;
-    int ik = i0;
-    int jk = j0;
-    int lk = l0;
-    int R02 = sqr(ik - Ci) + sqr(jk - Cj);
-    if (chek(R02, Rl, Rb, 0)){
-        return 0;
-    }
-    
-    for(int k = 1; k <= 50; k++){
-        int copyik = ik, copyjk = jk, copylk = lk;
-        ik = copyik*max(copyjk, copylk)%30 + copyjk*min(copyik, copylk)%20 + k;
-        jk = min(copyjk, max(copyjk,min(copylk, max(copyjk-copylk, copyik-copylk))));
-        lk = sign(k-10)*abs(copyik-copyjk+copylk-k);
-        int Rk2 = sqr(ik - Ci) + sqr((jk - Cj));
-        if(chek(Rk2, Rl, Rb, k)){
-            return 0;
+    //Тест из варианта
+    printf("First test from guide(Variant 29) i0 = %d, j0 = %d, l0 = %d\n Result =  ",-7, -19, 4); 
+    Calc(-7, -19, 4);//
+    //Далее идут 125 тестов для i0, j0, l0 = {1,...,5}
+    int k = 1;
+    for(int i0 = 1; i0 <= 5; i0++){
+        for(int j0 = 1; j0 <= 5; j0++){
+            for(int l0 = 1; l0 <= 5; l0++){
+                printf("Test N %d: i0 = %d, j0 = %d, l0 = %d\n Result = ", k, i0, j0, l0);
+                Calc(i0, j0, l0);
+                k++;
+            }
         }
     }
-    printf("No. k = 50, i = %d, j = %d, l = %d", ik, jk, lk);
+    
     return 0;
 }
