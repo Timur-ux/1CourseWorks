2b752b71566ef72cf76a5232657ad0a557b0a01e
TABuchkin <tabuchkin@mai.education>
2023-03-12 11:10:57 +0300

    work on 24 lab(2)

--- 2 semestr/Lab24/24.c
+++ 2 semestr/Lab24/24.c
@@ -2,13 +2,13 @@
 #include <malloc.h>
 #include <string.h>
 
-// typedef struct tree
-// {
-//     char* val;//'0' is number, '1' is letter, '+' or '-' or '/'... is operation
-//     struct tree* left;
-//     struct tree* right;
-//     struct tree* parent;
-// } Node;
+typedef struct tree
+{
+    char* val;//'0' is number, '1' is letter, '+' or '-' or '/'... is operation
+    struct tree* left;
+    struct tree* right;
+    struct tree* parent;
+} Node;
 
 typedef struct stack
 {
@@ -19,7 +19,8 @@ typedef struct stack
 Stack * create_stack(const char *val)
 {
     Stack *result = malloc(sizeof(Stack));
-    memcpy(result->val, val, strlen(val)*sizeof(char));
+    result->val = malloc(sizeof(char)*strlen(val)+1);
+    memcpy(result->val, val, strlen(val)*sizeof(char)+1);
     result->prev = NULL;
     return result;
 }
@@ -32,16 +33,19 @@ void Push(Stack **source, const char * val)// push new value on stack top;
         return;
     }
     Stack *result = malloc(sizeof(Stack));
-    memmove(result->val, val, strlen(val)*sizeof(char));
+    result->val = malloc(sizeof(char)*strlen(val)+1);
+    memmove(result->val, val, strlen(val)+1);
     result->prev = *source;
     (*source) = result;
 }
 
 const char * Pop(Stack **source)//pop first element from stack and return his value.
 {
-    char * result;
-    memcpy(result, (*source)->val, strlen((*source)->val));
-    (*source) = (*source)->prev;
+    char * result = malloc(strlen((*source)->val)+1);
+    memcpy(result, (*source)->val, strlen((*source)->val)+1);
+    Stack * prev = (*source)->prev;
+    free((*source));
+    (*source) = prev;
     return result;
 }
 
@@ -55,6 +59,15 @@ void printStack(Stack * stack)//Print stack from up to down.
     return;
 }
 
+const char * slice(const char * s, int a, int b)
+{
+    int len = b-a;
+    char * result = malloc(len+1);
+    memcpy(result, s+a, len);
+    *(result+len) = 0;
+    return result;
+}
+
 void sumStacks(Stack * A, Stack * B)//place stack A on stack B(A above B).
 {
     Stack * cur_stack = A;
@@ -65,47 +78,62 @@ void sumStacks(Stack * A, Stack * B)//place stack A on stack B(A above B).
     cur_stack->prev = B;
 }
 
-// int isOperand(const char * s)//return 1 if s is number or letter, return 0 otherwise
-// {
-//     if(*s[0] == '+' || *s[0] == '-' || *s[0] == '/' || *s[0] == '*')
-//     {
-//         return 0;
-//     }
-//     return 1;
-// }
-const char * slice(const char * s, int a, int b)
-{
-    int len = b - a;
-    char * result;
-    memmove(result, s+a, len);
-    return result;
-}
-
-void string2Stack(Stack ** dest, const char *s)
+void stringToStack(Stack **stack, const char *s)
 {
     int begin = 0;
-    printf("%s\n", slice(s, 0, 1));
-    for(int i = 0; i < strlen(s)+1; i++)
+    for(unsigned int i = 0; i < strlen(s)+1; i++)
     {
+        
         if(*(s+i) == ' ' || *(s+i) == '\0')
         {
-            // Push(dest, slice(s, begin, i));
+            Push(stack, slice(s, begin, i));
             begin = i + 1;
         }
     }
 }
+Stack * findLower(Stack *stack)
+{
+    Stack * cur_stack = stack;
+    while(cur_stack->prev != "(")
+    {
+        cur_stack = cur_stack->prev;
+    }
+    return cur_stack->prev;
+}
+Stack * convToPrefForm(Stack *cur_stack)
+{
+    Stack * result = NULL;
+    while(1)
+    {
+        val = Pop(&cur_stack);
+        if(val == "+" || val == "-" || val == "*" || val == "/")
+        {
+            Push(&result, val);
+        }
+        else if(val == ")")
+        {
+            Stack * inner_stack = convToPrefForm(&(cur_stack->prev));
 
-
-
-
+        }
+        else if (val == "(")
+        {
+            return result;
+        }
+        else
+        {
+            const char * op = Pop(&result);
+            Push(&result, val);
+            Push(&result, op);
+        }
+    }
+    
+}
 
 int main()
 {
-    const char *ss = "2 + 3";
-    printf("%s\n", slice(ss, 0, 1));
-    Stack * stack = create_stack(ss);
-    string2Stack(&stack, ss);
-    //printStack(stack);
+    const char *s = "( 2 + 3 ) * k + 174";
+    Stack * stack = NULL;
+    stringToStack(&stack, s);
     return 0;
 }
 //Какая-то лажа в slice когда она вызывается из функции. При вызове из main работает нормально.
